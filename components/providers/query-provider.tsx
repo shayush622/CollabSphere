83d456cb9f606da6a363595ff2629245849223dc
shayush622 <118544643+shayush622@users.noreply.github.com>
2025-03-10 23:34:02 +0530

    bugs fixed

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -2,6 +2,7 @@
 
 import {
     QueryClient,
+    
     QueryClientProvider
 } from "@tanstack/react-query";
 
@@ -10,7 +11,9 @@ import { useState } from "react";
 export const QueryProvider=({children}:{children: React.ReactNode})=>{
 
     const [queryClient]=useState(()=> new QueryClient());
+    
     return(
+        
         <QueryClientProvider client={queryClient}>
             
         {children}
